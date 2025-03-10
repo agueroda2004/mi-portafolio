ed0cab816d385a8a0a242db028577eac5ecc9d74
Daniel <agueroda2004@gmail.com>
2025-03-09 20:25:38 -0600

    chore(): se modifica el .gitignore y se agrega la carpeta img

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var myCarousel = new bootstrap.Carousel(document.querySelector("#carouselExampleRide"), {
+  var carouselAmeno = new bootstrap.Carousel(document.querySelector("#carouselAmeno"), {
       interval: 4000,
       ride: "carousel"
-    });
   });
+
+  var carouselCoolStreet = new bootstrap.Carousel(document.querySelector("#carouselCoolStreet"), {
+      interval: 4000,
+      ride: "carousel"
+  });
+});
+function flipCard(card) {
+  card.classList.toggle("flip");
+}
+
+
